1cfd6fded1706b91ee2c3d22469215b9e23f351d
Guhan <52208881+rookieman-22@users.noreply.github.com>
2019-07-07 23:51:17 +0530

    Update balanced_parentheses.cpp

--- balanced_parentheses.cpp
+++ balanced_parentheses.cpp
@@ -1,63 +1,54 @@
 #include<iostream>
-#include<stack>
 #include<string>
+#include<stack>
 using namespace std;
+bool ispair(char a,char b)
+{
+    if((a==']'&&b=='[')||(a=='}'&&b=='{')||(a==')'&&b=='('))
+        return true;
+    else
+        return false;
+}
 bool isbalanced(string exp)
 {
-    stack<char>s;
-    for(int i=0;i<exp.length();i++)
-    {
-       if(exp[i]=='{' || exp[i]=='['|| exp[i]=='(')
+     stack<char>s;
+     for(int i=0;i<exp.length();i++)
+     {
+         if(exp[i]=='{'||exp[i]=='['||exp[i]=='(')
+               {
+                   s.push(exp[i]);
+                   continue;
+               }
+         if(exp[i]=='}'||exp[i]==']'||exp[i]==')')
+         {
+             if(s.empty())
+              return false;
+            else
             {
-                s.push(exp[i]);
-            }
-        else if(exp[i]=='}')
-        {
-            if(s.top()=='{')
+             bool x=ispair(exp[i],s.top());
+                if(x==1)
                 {
                     s.pop();
                     continue;
                 }
-                else
-                {
-                    return false;
-                }
-        }
-        else if(exp[i]==']')
-        {
-            if(s.top()=='[')
-            {
-                s.pop();
-                continue;
-            }
-            else
+             else
                 return false;
-        }
-        else if(exp[i]==')')
-        {
-            if(s.top()=='(')
-            {
-                s.pop();
-                continue;
             }
-            else
-                return false;
         }
-        else
-            return false;
-    }
-    if(!s.empty())
+}
+ if(!s.empty())
         return false;
-    else
-        return true;
+     else return true;
 }
 int main()
 {
-    string exp;
-    cin>>exp;
-    bool y=isbalanced(exp);
-    if(y==1)
-        cout<<"String is balanced";
-    else
-        cout<<"String is not balanced";
+  string exp;
+  cout<<"Enter string: ";
+  cin>>exp;
+  bool y=isbalanced(exp);
+  if(y==1)
+    cout<<"String is balanced";
+  else
+    cout<<"String is not balanced";
 }
+
